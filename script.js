9877f2da0b374dbec3aa235a5a52f4e521ce3ecb
dejana-d <103682312+dejana-d@users.noreply.github.com>
2022-04-19 10:28:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// javascript file 
+
+
+function darkMode() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
+
+
+
+
+
+// do this bit later 
+
+/* test code to 'reveal' link only the date specified, like an advent calender
+code found here: https://stackoverflow.com/questions/33748648/how-can-i-hide-a-link-until-a-specific-date-and-time-arrives-html
+
+if ( new Date() >= new Date("Mon Nov 23 2015 9:00") ) { 
+   document.getElementById("links").innerHTML+= "<a href='target.html'>Some Link</a>";
+} */
